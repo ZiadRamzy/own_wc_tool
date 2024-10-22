c0cfa295a4d6114d6ba0b043a1628f8cc5a81c74
Ziad Abdelfatah <ge64cot@mytum.de>
2024-10-22 22:01:48 +0200

    Add support for -l option to count lines in cccwc. - Implemented line counting functionality using the new count_lines function. - Updated the main function to handle both -c (byte count) and -l (line count) options. - Improved error handling and usage instructions for command line arguments.

--- ccwc.py
+++ ccwc.py
@@ -13,23 +13,36 @@ def count_bytes(file_path: str) -> int:
         print(f" Error: File {file_path} not found.")
         sys.exit(1)
 
+def count_lines(file_path: str) -> int:
+    """Counts the number of lines in the specified file."""
+
+    try:
+        with open(file_path, "r") as file:
+            lines: List[str] = file.readlines()
+            return len(lines)
+    except FileNotFoundError:
+        print(f"Error: File {file_path} not found.")
+        sys.exit(1)
 
 
 def main(args: List[str]) -> None:
-    """Main function to handle command-line arguments and call the byte-counting function."""
+    """Main function to handle command-line arguments and call the appropriate counting function."""
 
     if len(sys.argv) != 3:
-        print("Usage ccwc -c <filename>")
+        print("Usage ccwc -c/-l <filename>")
         sys.exit(1)
 
     option = sys.argv[1]
     file_path = sys.argv[2]
 
     if option == "-c":
-        byte_count = count_bytes(file_path)
+        byte_count: int = count_bytes(file_path)
         print(f" {byte_count}  {os.path.basename(file_path)}")
+    elif option == "-l":
+        line_count: int = count_lines(file_path)
+        print(f" {line_count} {os.path.basename(file_path)}")
     else:
-        print("Error: Unsupported option. Only -c is supported.")
+        print("Error: Unsupported option. Use -c for byte count or -l for line count.")
         sys.exit(1)
 
 if __name__ == "__main__":
