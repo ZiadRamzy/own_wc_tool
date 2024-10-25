2fd7bb5112c2cdf90219a783c74192c33f9c7da8
Ziad Abdelfatah <ge64cot@mytum.de>
2024-10-25 04:06:05 +0200

    Add default option support to ccwc for counting lines, words and bytes - Implemented default behavior for ccwc where no flags are provided. - The default option counts and outputs the number of lines, words and bytes in the file.

--- ccwc.py
+++ ccwc.py
@@ -54,28 +54,36 @@ def count_characters(file_path: str) -> int:
 def main(args: List[str]) -> None:
     """Main function to handle command-line arguments and call the appropriate counting function."""
 
-    if len(args) != 3:
-        print("Usage ccwc -c/-l/-w/-m <filename>")
+    if len(args) < 2 or len(args) > 3:
+        print("Usage ccwc [-c/-l/-w/-m] <filename>")
         sys.exit(1)
 
-    option = args[1]
-    file_path = args[2]
-
-    if option == "-c":
-        byte_count: int = count_bytes(file_path)
-        print(f" {byte_count}  {os.path.basename(file_path)}")
-    elif option == "-l":
+    if len(args) == 2: # No option provided
+        file_path: str = args[1]
         line_count: int = count_lines(file_path)
-        print(f" {line_count} {os.path.basename(file_path)}")
-    elif option == "-w":
         word_count: int = count_words(file_path)
-        print(f" {word_count} {os.path.basename(file_path)}")
-    elif option == "-m":
-        character_count: int = count_characters(file_path)
-        print(f" {character_count} {os.path.basename(file_path)}")
+        byte_count: int = count_bytes(file_path)
+        print(f" {line_count} {word_count} {byte_count} {os.path.basename(file_path)}")
+
     else:
-        print("Error: Unsupported option. Use -c for byte count, -l for line count, -w for word count or -m for character count.")
-        sys.exit(1)
+        option = args[1]
+        file_path = args[2]
+
+        if option == "-c":
+            byte_count: int = count_bytes(file_path)
+            print(f" {byte_count}  {os.path.basename(file_path)}")
+        elif option == "-l":
+            line_count: int = count_lines(file_path)
+            print(f" {line_count} {os.path.basename(file_path)}")
+        elif option == "-w":
+            word_count: int = count_words(file_path)
+            print(f" {word_count} {os.path.basename(file_path)}")
+        elif option == "-m":
+            character_count: int = count_characters(file_path)
+            print(f" {character_count} {os.path.basename(file_path)}")
+        else:
+            print("Error: Unsupported option. Use -c for byte count, -l for line count, -w for word count or -m for character count.")
+            sys.exit(1)
 
 if __name__ == "__main__":
     main(sys.argv)
